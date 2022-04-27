e1fe6355799baf2962cc7b1bd48e273f8ae69c79
Nikita <sominola@gmail.com>
2022-04-27 14:48:48 +0300

    add Migration

---
+++ src/DataLayer/CVBuilder.EFContext/Migrations/20220427111535_AddCvNameField.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CVBuilder.EFContext.Migrations
+{
+    public partial class AddCvNameField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "CVs",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "CVs",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CvName",
+                table: "CVs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CvName",
+                table: "CVs");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "CVs",
+                newName: "Surname");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "CVs",
+                newName: "Name");
+        }
+    }
+}
