3b2e0530fabcdeadaf84986db4e04bd760bf359c
Nikita <sominola@gmail.com>
2022-05-04 15:48:56 +0300

    change name

--- src/CVBuilder.Web/Contracts/V1/Responses/CV/CvCardResponse.cs
+++ src/CVBuilder.Web/Contracts/V1/Responses/CV/CvCardResponse.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string CvName { get; set; }
         public bool IsDraft { get; set; }
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Picture { get; set; }
     }
