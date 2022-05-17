80abd960cde2411a41eb15f6307e3afb9ce45e66
Nikita <sominola@gmail.com>
2022-05-17 12:45:05 +0300

    change url

--- src/CVBuilder.Application/CV/Handlers/GetPdfByIdHandler.cs
+++ src/CVBuilder.Application/CV/Handlers/GetPdfByIdHandler.cs
@@ -31,7 +31,7 @@ namespace CVBuilder.Application.CV.Handlers
             var browser = _browserExtension.Browser;
             await using var page = await browser.NewPageAsync();
             await page.EvaluateExpressionOnNewDocumentAsync($"window.localStorage.setItem('JWT_TOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnaXZlbl9uYW1lIjoiICIsImVtYWlsIjoibnNhdmNodWsyMjRAZ21haWwuY29tIiwianRpIjoiNTVkNzMxMTQtMGVhMi00Y2MwLWE4ZjItNmVjOWRhNDk1NmExIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiYTJmYmRlMTUtZTRiNS00ZmUwLWI3NzctMGIxM2NlYWNhNGU3IiwibmFtZWlkIjoiMiIsInJvbGUiOiJBZG1pbiIsIm5iZiI6MTY1MjY0MDU5MywiZXhwIjoxNjYxMjg0MTkzLCJpYXQiOjE2NTI2NDA1OTN9.j60cyTOSjehhfClc_h7WOzIY-eMZjBWX5MS1ckyVqCo');");
-            await page.GoToAsync($"https://cvbuilder-front-c4x1i5jhw-sominola.vercel.app/home/cv/{request.ResumeId}");
+            await page.GoToAsync($"https://cvbuilder-front.vercel.app/home/cv/{request.ResumeId}");
             await Task.Delay(2000, cancellationToken);
             await page.EvaluateExpressionAsync(
                 @"var doc = document.getElementById(""doc"");document.body.innerHTML = '';document.body.appendChild(doc);");
