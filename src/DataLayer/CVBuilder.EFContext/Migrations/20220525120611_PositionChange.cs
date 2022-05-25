d8452f75e60fba65c4ec0b91bf6856fefbfe35f7
Nikita <sominola@gmail.com>
2022-05-25 15:19:00 +0300

    add migration

---
+++ src/DataLayer/CVBuilder.EFContext/Migrations/20220525120611_PositionChange.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVBuilder.EFContext.Migrations
+{
+    public partial class PositionChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSelected",
+                table: "TeamResume");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusResume",
+                table: "TeamResume",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusResume",
+                table: "TeamResume");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSelected",
+                table: "TeamResume",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
