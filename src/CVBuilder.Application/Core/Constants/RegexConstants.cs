05e40c00f35d4dc07b95a61fdf54a335216ac1c4
Nikita <sominola@gmail.com>
2022-07-13 17:26:38 +0300

    fix

--- src/CVBuilder.Application/Core/Constants/RegexConstants.cs
+++ src/CVBuilder.Application/Core/Constants/RegexConstants.cs
@@ -3,7 +3,8 @@
 	public static class RegexConstants
 	{
         public const string PHONE_ALL_REGEX = "^$|[0-9 ()+-]+";
-        public const string SITE_REGEX = "^$(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?";
+        public const string SITE_REGEX = "^$|(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?";
+
 
 	}
 }
